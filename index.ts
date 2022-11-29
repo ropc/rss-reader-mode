390b57443eb0c48c7a35b1c1a7ec109a71839e8c
ropc <1666878+ropc@users.noreply.github.com>
2022-11-28 23:59:30 -0500

    working with root rss feed

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { XMLParser } from 'fast-xml-parser';
 import { JSDOM } from 'jsdom';
 import { Readability } from '@mozilla/readability';
 import { render } from 'mustache';
@@ -6,11 +7,34 @@ import { readFile } from 'node:fs/promises';
 
 const app = express();
 
-app.get('/rss/:root', async (req, res) => {
-    console.log('ohai', req.url, req.params);
-    const response = await fetch(req.params.root);
+interface RSSChannel {
+    title: string;
+    link: string;
+    description: string;
+    item: RSSItem[];
+}
+
+interface RSSItem {
+    title: string;
+    link: string,
+    guid?: string,
+    pubDate: string,
+    description: string;
+}
+
+interface ItemView extends RSSItem {
+    author: string;
+    content: string;
+}
+
+// TODO: cache items
+const fetchItem = async (item: Partial<ItemView> & { link: string }): Promise<ItemView | undefined> => {
+    const { link } = item;
+
+    console.log('fetching', link);
+    const response = await fetch(link);
     const content = await response.text();
-    // console.log('response', response.headers, content);
+    console.log('response', response.headers, content);
     const dom = new JSDOM(content);
     // console.log('dom', dom);
     const reader = new Readability(dom.window.document);
@@ -19,25 +43,40 @@ app.get('/rss/:root', async (req, res) => {
     console.log(JSON.stringify(parseResult, null, 2));
 
     if (!parseResult) {
-        res.sendStatus(500);
         return;
     }
 
+    return {
+        ...parseResult,
+        description: parseResult.excerpt,
+        pubDate: new Date().toISOString(),
+        author: parseResult.byline,
+        ...item
+    };
+};
+
+// assumes given url is a valid RSS feed
+app.get('/rss/:root_rss', async (req, res) => {
+    console.log('ohai', req.url, req.params);
+    const response = await fetch(req.params.root_rss);
+    const content = await response.text();
+    // console.log('response', response.headers, content);
+
+    const xml: { rss: { channel: RSSChannel } } = new XMLParser().parse(content);
+    console.log(xml);
+    const rssItems = xml.rss.channel.item.slice(0, 10);
+
+    console.log('items', rssItems);
+
+    const unfilteredItemViews = await Promise.all(rssItems.map(fetchItem));
+    const items = unfilteredItemViews.filter((x): x is ItemView => !!x);
+
     const template = await readFile('feed.mustache', 'utf8');
     const feedView = {
-        title: req.params.root,
-        homepage_url: req.params.root,
-        rss_url: req.params.root,
-        items: [
-            {
-                ...parseResult,
-                description: parseResult.excerpt,
-                url: req.params.root,
-                date: new Date(),
-                author: parseResult.byline,
-
-            }
-        ]
+        title: xml.rss.channel.title || req.params.root_rss,
+        homepage_url: xml.rss.channel.link || req.params.root_rss,
+        rss_url: req.params.root_rss,
+        items
     };
 
     res.send(render(template, feedView));
